9d76e74ce7d5646c5b6c9cba2d25120c58d4bcb3
AquamanRanda <naveeng2711@gmail.com>
2022-03-15 06:00:30 +0530

    Added Aes

--- src/cnsprograms/caesarCipher.py
+++ src/cnsprograms/caesarCipher.py
@@ -10,6 +10,19 @@ def encrypt(text,s):
       else:
         result += chr((ord(char) + s - 97) % 26 + 97)
   return result
+
+def decrypt(text,s):
+  result = ""
+    # transverse the plain text
+  for i in range(len(text)):
+      char = text[i]
+        # Encrypt uppercase characters in plain text
+      if (char.isupper()):
+        result += chr((ord(char) - s-65) % 26 + 65)
+      # Encrypt lowercase characters in plain text
+      else:
+        result += chr((ord(char) - s - 97) % 26 + 97)
+  return result
 #check the above function
 text = "CEASERCIPHERDEMO"
 s = 4
@@ -17,3 +30,4 @@ s = 4
 print("Plain Text : " + text)
 print ("Shift pattern : " + str(s))
 print("Cipher: " + encrypt(text,s))
+print("decryption: " + decrypt(encrypt(text,s),s))
